6bdfcbeb5754b8ecdad0d1759774a77e7ea267d6
Mert48Ta <113031249+Mert48Ta@users.noreply.github.com>
2025-04-06 15:15:21 +0300

    Fix: Remove unused icon imports

--- src/App.js
+++ src/App.js
@@ -12,16 +12,8 @@ import {
   PhoneIcon, 
   EnvelopeIcon, 
   MapPinIcon, 
-  LightBulbIcon,
-  SunIcon, 
-  TrophyIcon, 
-  ShieldCheckIcon, 
-  ArrowTrendingUpIcon,
-  ChatBubbleLeftRightIcon, 
-  HandThumbUpIcon,
   ChevronDownIcon,
-  BoltIcon,
-  UserCircleIcon
+  BoltIcon
 } from '@heroicons/react/24/outline';
 import { 
   LightBulbIcon as LightBulbIconSolid,
